5055871c1f280a71905d29d6d585e8aa5c8a2456
salomon-tech <salomondev7@gmail.com>
2025-12-11 23:00:37 +0200

    initial commit

---
+++ app/todo/todo/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Todo(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    completed = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
--- app/todo/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-from .models import Task
-
-
-# Create your views here.
